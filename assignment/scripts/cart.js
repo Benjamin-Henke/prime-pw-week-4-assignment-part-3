12d92b432b02a52f8e3d73470264867d67709c2d
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-01 20:39:48 -0500

    Wrote function to empty basket and tested successfully

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -20,7 +20,7 @@ function listItems(item) {
 // Call functions to test:
 console.log('Empty basket:', basket);
 
-console.log('adding eggs to basket:', addItem('apple'));
+console.log('adding eggs to basket:', addItem('eggs'));
 console.log('Basket is:', basket);
 
 console.log('adding bacon to basket:', addItem('bacon'));
@@ -31,3 +31,30 @@ console.log('Basket is:', basket);
 
 console.log('adding milk to basket:', addItem('milk'));
 console.log('Basket is:', basket);
+
+// function empty will reset the basket to 0 using the array length property
+function empty() {
+  basket.length = 0;
+} // end empty function
+empty(basket); // emptying the basket
+
+// Call function to test
+console.log("Empting the basket:", basket);
+
+// Here we can add new items to the basket
+console.log('New basket of groceries:');
+
+console.log('adding cherries to cherries:', addItem('cherries'));
+console.log('Basket is:', basket);
+
+console.log('adding steaks to basket:', addItem('steaks'));
+console.log('Basket is:', basket);
+
+console.log('adding spinach to basket:', addItem('spinach'));
+console.log('Basket is:', basket);
+
+console.log('adding salad dressing to basket:', addItem('salad dressing'));
+console.log('Basket is:', basket);
+
+empty(basket); // emptying out the new basket
+console.log('Emptying the basket:', basket);
